a7d972f78019049b336ee35e66b4eb047899a325
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-09 23:07:30 +0900

    propsでchildrenで値を渡す

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export const App = () => {
   return (
     <div>
       <h1 style={{color: "red"}}>hello, world!</h1>
-      <ColoredMessage color="blue" message="howdy?" />
-      <ColoredMessage color="pink" message="hogefuga" />
+      <ColoredMessage color="blue">howdy?</ColoredMessage>
+      <ColoredMessage color="pink">hogefuga</ColoredMessage>
       <button onClick={onClickButton}>アラートボタン</button>
     </div>
   );
