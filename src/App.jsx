efb9d569a0da0060a4959fb2ad012db65ae28473
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-09 22:52:26 +0900

    style適用

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ export const App = () => {
   const onClickButton = () => {
     alert();
   }
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px"
+  }
   return (
     <div>
-      <h1>hello, world!</h1>
-      <p>hogefuga</p>
+      <h1 style={{color: "red"}}>hello, world!</h1>
+      <p style={contentStyle}>hogefuga</p>
       <button onClick={onClickButton}>アラートボタン</button>
     </div>
   );
