af83df7b3363cd708184c39f03aa9c68a533bfd8
Makotox0207 <xiaoxishicheng@gmail.com>
2023-10-09 22:49:41 +0900

    アラートボタン

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  }
   return (
     <div>
       <h1>hello, world!</h1>
       <p>hogefuga</p>
+      <button onClick={onClickButton}>アラートボタン</button>
     </div>
   );
 };
